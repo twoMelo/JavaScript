db6f68e2d82314d54348b43e853b02eb58abe4f4
twoMelo <mmeloramalho@gmail.com>
2025-01-29 15:23:45 -0300

    Update

--- Operadores/if.js
+++ Operadores/if.js
@@ -1,3 +1,8 @@
 // Estrutura condicional - if else
 let idade = prompt("Qual sua idade?")
 console.log("Sua idade é? ", idade)
+
+let numero = 7
+if (numero > 10) {
+    console.log("é maior que 10")
+}
