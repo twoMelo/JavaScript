9893faa8d5e8c0334f021c4cd6524965c8ff95f9
twoMelo <mmeloramalho@gmail.com>
2025-01-29 08:19:08 -0300

    Initial commit

---
+++ Operadores/aritmeticos.js
@@ -0,0 +1,27 @@
+// console.log("Hello World!")
+
+// para declarar variaveis
+let nome = "melo" //vamos preferir esse
+var nomes = "melo"
+
+// CTRK + ; -> COMENTARIOS
+console.log(nome)   
+
+let idade = 17
+console.log("Minha idade é ", idade, "anos")
+
+//OPERAÇÕES MATEMÁTICAS
+let a
+let b
+
+let soma = a + b 
+let sub = a - b 
+let mult = a * b
+let div = a / b
+let resto = a % b
+
+console.log(soma)
+console.log(sub)
+console.log(mult)
+console.log(div)
+console.log(resto)
---
+++ Operadores/lista-01.js
@@ -0,0 +1,8 @@
+let a 
+let b
+
+let soma = a + b 
+let sub = a - b 
+let mult = a * b
+let div = a / b
+let resto = a % b
