dd4f15dd42153d7ef4754540e9d6a02aba206c5f
twoMelo <mmeloramalho@gmail.com>
2025-02-05 11:08:55 -0300

    .

--- Estrutura condicional/logicos.js
+++ Estrutura condicional/logicos.js
@@ -1,27 +1,34 @@
-//verifica se a pessoa pode votar
-let idade = 20
+// Verificar se a pessoa pode votar
+let idade = 18
 let tituloEleitor = true
 
-if(idade >=18 && tituloEleitor == true) {
+if(idade >= 18 && tituloEleitor == true) {
     console.log("Pode votar")
-} else{
+} else {
     console.log("Não pode votar")
 }
 
-//Verificar se o número está dentro do intervalo
-let numero = 15
-if (numero >= 10 && numero <= 20 ){
-    console.log("O numero esta entre 10 e 20")
+// Verificar se o número está dentro do intervalo
+let numero = 21
+if (numero >= 10 && numero <= 20) {
+    console.log("O número está entre 10 e 20")
 } else {
-    console.log("O numero esta fora do intervalo")
+    console.log("O número está fora do intervalo")
 }
 
-//Compra com desconto
+// Compra com desconto
 let valor = 150
 let clienteVip = false
-
-if(valor >= 100 || clienteVip == true){
-    console.log("Voce ganhou frete grátis")
+if(valor >= 100 || clienteVip == true) {
+    console.log("Você ganhou frete grátis")
 } else {
-    console.log("Tem que pagar o frete")
+    console.log("Tem que pagar frete")
+}
+
+// Número no intervalo
+let num = 10
+if (num >= 5 || num <= 20) {
+    console.log("Funcionou")
+} else {
+    console.log("Não funcionou")
 }
---
+++ Exercicios/lista-05.js
@@ -0,0 +1,53 @@
+// Questao 1
+let idade = prompt("Qual sua idade?")
+if (idade < 18) {
+    console.log("Você não pode dirigir")
+} else if (idade > 18) {
+    console.log("Você pode dirigir")
+}
+
+// Questao 2
+let anosTrabalho = prompt("Quantos anos você trabalhou na empresa?")
+let projetos = prompt("Quantos projetos você realizou?")
+if (anosTrabalho >= 5 || projetos > 10) {
+    console.log("Você está elegível para promoção")
+} else {
+    console.log("Você não está elegível para promoção")
+}
+
+// Questao 3
+let idade = prompt("Qual sua idade?")
+if (idade >= 18 && idade <= 30){
+    console.log("Você pode entrar no evento")
+} else {
+    console.log("Voce não pode entrar no evento")
+}
+
+// Questao 4
+let idade = prompt("Qual sua idade?")
+if (idade > 21) {
+    console.log("Você foi aceito para a vaga")
+} else if (idade < 21) {
+    console.log("Você não foi aceito para a vaga")
+}
+
+// Questao 5
+let nome = prompt("Qual o seu user name")
+let senha = prompt("Digite a senha")
+if (nome == "admin" && "1234") {
+    console.log("Login bem-sucedido")
+} else if {
+    console.log("Nome de usuário ou senha incorretos")
+}
+
+// Questao 6
+let numero = prompt("Numeros")
+if (numero > 10 && numero <=20){
+    console.log("O número está dentro do intervalo entre 10 e 20")
+} else if (numero >= 30 && numero <=50){
+    console.log ("O número está dentro do intervalo entre 30 e 50")
+} else {
+    console.log("nao esta ")
+}
+
+// Questao 7
