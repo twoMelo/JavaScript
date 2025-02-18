b79f67c8f196d112e1b760af709a582a3d9b21a0
twoMelo <mmeloramalho@gmail.com>
2025-02-18 14:53:12 -0300

    .

---
+++ Exercicios/lista-11.js
@@ -0,0 +1,9 @@
+// Exercicios 1
+
+let filmes = ["O Sonhor dos Anéis", "Matrix", "Inception"]
+console.log(filmes[0])
+
+// Exercicios 2
+
+let frutos = ["Banana", "Maçã","Morango", "Uva", "Pera"]
+console.log(frutos[2])
