04457f77cdd747bac7e3120855d073b1255a26c2
twoMelo <mmeloramalho@gmail.com>
2025-02-19 09:52:36 -0300

    .

--- Exercicios/test.js
+++ Exercicios/test.js
@@ -1,4 +1,11 @@
-function calcularAreaRetangulo(largura, altura) {
-  return largura * altura;
+function compararIdade (idade1, idade2)
+{
+    if (idade1 > idade2) {
+        console.log("A primeira pessoa é mais velha")
+    } else if (idade2 > idade1) {
+        console.log("A segunda pessoa é mais velha")
+    } else {
+        console.log("As duas pessoas têm a mesma idade")
+    }
 }
-console.log(calcularAreaRetangulo(5, 10));
+compararIdade(15, 30)
