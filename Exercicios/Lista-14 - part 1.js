926119266d6189ef759f7963b1d33fc5c36c00bb
twoMelo <mmeloramalho@gmail.com>
2025-02-26 13:30:48 -0300

    .

--- Exercicios/Lista-14 - part 1.js
+++ Exercicios/Lista-14 - part 1.js
@@ -1,9 +1,13 @@
 // Exercicio 1
 
-document.getElementById('meuBotao').addEventListener('click', function() {
+document.getElementById('botao').addEventListener('click', function() {
     var paragrafo = document.getElementById('meuParagrafo');
     paragrafo.style.color = paragrafo.style.color === 'red' ? 'black' : 'red';
 });
 
 // Exercicio 2
 
+document.getElementById('botao').addEventListener('click', function() {
+    var botao = document.getElementById('botao');
+    botao.style.backgroundColor = botao.style.backgroundColor === 'blue' ? 'green' : 'blue';
+});
