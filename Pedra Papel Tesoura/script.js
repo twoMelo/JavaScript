83e2b997f36c190a9f2043b511f5ad9dc5673f54
twoMelo <mmeloramalho@gmail.com>
2025-03-25 10:35:53 -0300

    .

--- Pedra Papel Tesoura/script.js
+++ Pedra Papel Tesoura/script.js
@@ -33,3 +33,31 @@ function jogar(escolha) {
         ganhador = 0;
     }
 }    
+
+let jogadorEscolha1 = document.getElementById("jogadorEscolha1")
+let jogadorEscolha2 = document.getElementById("jogadorEscolha2")
+let jogadorEscolha3 = document.getElementById("jogadorEscolha3")
+let computadorEscolha1 = document.getElementById("computadorEscolha1")
+let computadorEscolha2 = document.getElementById("computadorEscolha2")
+let computadorEscolha3 = document.getElementById("computadorEscolha3")
+
+jogadorEscolha1.classList.remove('selecionado')
+jogadorEscolha2.classList.remove('selecionado')
+jogadorEscolha3.classList.remove('selecionado')
+computadorEscolhaEscolha1.classList.remove('selecionado')
+computadorEscolhaEscolha2.classList.remove('selecionado')
+computadorEscolhaEscolha3.classList.remove('selecionado')
+
+document.getElementById("jogadorEscolha" + jogadorEscolha).classList.add('selecionado')
+document.getElementById("computadorEscolha" + computadorEscolha).classList.add('selecionado')
+
+let mensagem = document.getElementById("mensagem")
+if (ganhador == 0) {
+    mensagem.innerHTML = "Empate"
+} else if (ganhador == 1) {
+    mensagem.innerHTML = "Jogador Ganhou"
+    jogadorPontos++
+} else if (ganhador == 2) {
+    mensagem.innerHTML = "Computador Ganhou"
+    computadorPontos++
+}
