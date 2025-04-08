e0dc44978b749fe8b70db357d6c8fa0a4f5bc9c7
twoMelo <mmeloramalho@gmail.com>
2025-04-08 09:02:06 -0300

    .

--- Cam/script.js
+++ Cam/script.js
@@ -2,3 +2,10 @@
 const video = document.getElementById("video")
 
 navigator.mediaDevices.getUserMedia({ video: true })
+    .then(stream => {
+        video.srcObject = stream
+        video.play()
+    })
+    .catch(error => {
+        console.error("Erro ao acessar a câmera: ", error)
+    })
